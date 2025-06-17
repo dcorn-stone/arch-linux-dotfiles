15478dadb035773d416c97490a48f6d889836d68
dcorn-stone <gayi_chen@outlook.com>
2025-06-17 18:23:11 +0800

    Disable auto format in neovim

---
+++ .config/nvim/nvim/init.lua
@@ -0,0 +1,16 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false, -- Show files that begin with "."
+      hide_gitignored = false, -- Show files that are normally ignored by git
+      -- Optionally, you can also add or clear other filters:
+      hide_by_name = {}, -- List of filenames to hide (empty here)
+      never_show = {}, -- List of filenames to never show
+    },
+    use_libuv_file_watcher = true, -- Automatically update the tree when files/directories change
+  },
+  -- You can add configurations for buffers, git_status, etc., if needed.
+})
